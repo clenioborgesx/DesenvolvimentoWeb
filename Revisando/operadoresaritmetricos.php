b9fe452ec2424d4dab458357b78d46d5e778d018
Clênio Borges <54286827+clenioborgesx@users.noreply.github.com>
2022-10-10 10:15:45 -0300

    Revisando
    Dia de treino

---
+++ Revisando/operadoresaritmetricos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se e Senão</title>
+</head>
+<body>
+    <h1>Somatório</h1>
+    <?php
+        $a = 2;
+        $b = 3;
+        $c=$a+$b;
+        echo "<h2>$a + $b = $c</h2>";
+    ?>
+    <h1>Divisão</h1>
+    <?php
+        $c=$b/$a;
+        echo "<h2>$b / $a = $c</h2>";
+    ?>
+</body>
+</html>
